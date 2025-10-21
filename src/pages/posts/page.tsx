a8bd3cdd7810489125b2dcb5615979f08d8e9b57
neominal.solutions <neominal.solutions@gmail.com>
2025-10-21 14:18:26 +0300

    Antd DataGrid

--- src/pages/posts/page.tsx
+++ src/pages/posts/page.tsx
@@ -25,7 +25,13 @@ function PostPage() {
 
 	const [searchText, setSearchText] = React.useState('');
 	const [searchedColumn, setSearchedColumn] = React.useState('');
+	// html elementinin referansını almak için kullandığımız bir hook.
+	// state değişse bile useRef bir sonraki renderda değerini korur.
 	const searchInput = React.useRef<InputRef>(null);
+
+	// state değişse bile arkaplanda değerleri korumak veya bir değerin önceki sonraki durumunu elimizde tutmak için useRefden yaralanıyoruz.
+	// Html elementlerin input,button -> trigger focus, click., getValue
+	// const filterCountRef = React.useRef<number>(0);
 
 	// ilk render-> postState.length 0
 	// compnentin ilk mount olduğu ve unmonut olana kadar sadece 1 kez doma yüklendği hook.
@@ -117,6 +123,9 @@ function PostPage() {
 			//console.log('record', record);
 			//console.log('value', value);
 			//console.log('dataIndex', dataIndex);
+			// herbir arama işleminde
+			// filterCountRef.current = filterCountRef.current + 1;
+			// console.log('filterCountRef', filterCountRef.current);
 
 			return record[dataIndex]
 				.toString()
@@ -125,8 +134,9 @@ function PostPage() {
 		},
 		filterDropdownProps: {
 			onOpenChange(open) {
+				console.log('open-Chanage');
 				if (open) {
-					setTimeout(() => searchInput.current?.select(), 100);
+					setTimeout(() => searchInput.current?.focus(), 100);
 				}
 			},
 		},
@@ -185,11 +195,22 @@ function PostPage() {
 	return (
 		<>
 			<div style={{ padding: 5 }}>
-				<p>searchedColumn: {searchedColumn}</p>
-
-				<p>searchInput: {searchText}</p>
-
-				<Table<PostModel> columns={columns} dataSource={postState} />
+				{/* <p>Search Input {searchedColumn}</p>
+				<p>Filtreleme Sayısı: {filterCountRef.current}</p> */}
+				<Table<PostModel>
+					pagination={{
+						showSizeChanger: true,
+						pageSizeOptions: [5, 10, 15, 20, 50],
+						defaultPageSize: 5,
+						locale: { items_per_page: 'adet/sayfa' },
+						showTotal(total, range) {
+							console.log('range', range);
+							return `${range[1]} / ${total} kayıt`; // 100 kayıtta ilk 5 kayıt gösteriyorum.
+						},
+					}}
+					columns={columns}
+					dataSource={postState}
+				/>
 			</div>
 		</>
 	);
